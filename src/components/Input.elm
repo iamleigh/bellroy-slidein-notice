63ccb55f85fbc7c4aaaadf1bd0348ea7e62df06c
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-04-18 03:33:13 -0500

    feat(input): Create new component
    This commit creates a basic input element.

---
+++ src/components/Input.elm
@@ -0,0 +1,34 @@
+module Components.Input exposing (InputConfig, input)
+
+import Html exposing (Html, input)
+import Html.Attributes exposing (class, placeholder, type_, value)
+import Html.Events exposing (onInput)
+
+
+
+-- TYPES
+
+
+type alias InputConfig msg =
+    { inputType : String
+    , placeholderText : String
+    , value : String
+    , onInput : String -> msg
+    , classes : List String
+    }
+
+
+
+-- VIEW
+
+
+input : InputConfig msg -> Html msg
+input config =
+    Html.input
+        [ class (String.join " " config.classes)
+        , type_ config.inputType
+        , placeholder config.placeholderText
+        , value config.value
+        , onInput config.onInput
+        ]
+        []
