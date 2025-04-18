2556209a415daa3691d693b2eed9a6caa8e27bce
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-04-18 13:45:54 -0500

    chore: Move products to its own component

---
+++ src/Components/Product.elm
@@ -0,0 +1,13 @@
+module Components.Product exposing (viewProduct)
+
+import Html exposing (Html, div, p, text)
+import Html.Attributes exposing (class)
+import Models.Product exposing (Product)
+
+
+viewProduct : Product -> Html msg
+viewProduct product =
+    div [ class "bellroy-product" ]
+        [ p [] [ text product.name ]
+        , p [] [ text ("$" ++ product.price) ]
+        ]
--- src/Layouts/Body.elm
+++ src/Layouts/Body.elm
@@ -3,6 +3,7 @@ module Layouts.Body exposing (view)
 import Html exposing (Html, button, div, p, text)
 import Html.Attributes exposing (class, disabled)
 import Html.Events exposing (onClick)
+import Components.Product exposing (viewProduct)
 import Models.Product exposing (Product)
 
 
@@ -12,11 +13,11 @@ import Models.Product exposing (Product)
 view : List Product -> msg -> Bool -> Html msg
 view products showNoticeMsg noticeVisible =
     div [ class "bellroy-body" ]
-        [ div [ class "products" ]
-            (List.map viewProduct products)
-
-        , p []
+        [ p []
             [ text "Welcome to Bellroy! We craft slim wallets, stylish bags, and accessories to help you move smarter through the world." ]
+
+        , div [ class "products" ]
+            (List.map viewProduct products)
 
         , button
             [ class "bellroy-button"
@@ -24,12 +25,4 @@ view products showNoticeMsg noticeVisible =
             , disabled noticeVisible
             ]
             [ text "Show Notice Again" ]
-        ]
-
-
-viewProduct : Product -> Html msg
-viewProduct product =
-    div [ class "product-item" ]
-        [ p [] [ text product.name ]
-        , p [] [ text ("$" ++ product.price) ]
         ]
