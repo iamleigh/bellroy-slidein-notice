34a3bef28b163611499bdf3452484324e070730f
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-04-18 03:32:31 -0500

    feat(button): Create new component
    This commit creates a new button component that supports:
    - Labels
    - Icons (from FontAwesome)
    
    It also allows icon position configuration to place it before (left) or after (right) the label; or simply have an icon button without visual label.

---
+++ src/components/Button.elm
@@ -0,0 +1,64 @@
+module Components.Button exposing (ButtonConfig, IconPosition(..), uiButton)
+
+import Html exposing (Html, button, span, text)
+import Html.Attributes exposing (attribute, class)
+import Html.Events exposing (onClick)
+
+
+-- TYPES
+
+
+type IconPosition
+    = IconLeft
+    | IconRight
+
+
+type alias ButtonConfig msg =
+    { label : String
+    , iconName : Maybe String
+    , iconPosition : Maybe IconPosition
+    , onClick : msg
+    , classes : List String
+    }
+
+
+-- VIEW
+
+
+uiButton : ButtonConfig msg -> Html msg
+uiButton config =
+    let
+        iconView maybeIconName =
+            case maybeIconName of
+                Just name ->
+                    span
+                        [ class ("fa-solid fa-xs fa-" ++ name)
+                        , attribute "aria-hidden" "true"
+                        ]
+                        []
+
+                Nothing ->
+                    text ""
+
+        labelView =
+            span [ class "button-label" ] [ text config.label ]
+
+        contents =
+            case ( config.iconName, config.iconPosition ) of
+                ( Just _, Just IconLeft ) ->
+                    [ iconView config.iconName, labelView ]
+
+                ( Just _, Just IconRight ) ->
+                    [ labelView, iconView config.iconName ]
+
+                ( Just _, Nothing ) ->
+                    [ iconView config.iconName ]
+
+                ( Nothing, _ ) ->
+                    [ labelView ]
+    in
+    button
+        [ class (String.join " " config.classes)
+        , onClick config.onClick
+        ]
+        contents
